35ac0cd8757a5bdabd18e5253fea063bbf91375d
Blake Wilson <blake@yellowpapersun.com>
2019-06-17 20:27:46 -0700

    log num bytes before and after compressing

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,15 @@ fn main() {
     // let txt = print_tree(&root);
     // println!("{}", txt);
     // println!("{:?}", hm);
-
-    // build_huffman_tree("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
-    let (encoded, root) =
-        huffman::build_huffman_tree("Huffman coding is a data compression algorithm.");
+    // let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    let text = "Huffman coding is a data compression algorithm.";
+    let (encoded, root) = huffman::build_huffman_tree(text);
     println!("encoded string: {}", encoded);
     let decoded = huffman::decode_huffman_tree(&encoded, &root);
     println!("decoded string: {}", decoded);
+    println!(
+        "compressed from {} bytes to {}",
+        text.len(),
+        encoded.len() / 8
+    );
 }
