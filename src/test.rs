aa4100aa7b0e116ae11c2b4c50b756fcecc2ad03
Blake Wilson <blake@launchdarkly.com>
2020-04-04 18:03:42 -0700

    Fix test

--- src/test.rs
+++ src/test.rs
@@ -3,10 +3,15 @@ mod huffman;
 #[cfg(test)]
 mod tests {
     use super::huffman;
+    use std::collections::HashMap;
+    use std::str::from_utf8;
     #[test]
     fn test_decode_huffman_tree() {
         let txt = "Huffman coding is a data compression algorithm.";
-        let (encoded, root) = huffman::build_huffman_tree(txt);
-        assert_eq!(txt, huffman::decode_huffman_tree(&encoded, &root));
+        let hm = &mut HashMap::new();
+        let (encoded, root) = huffman::build_huffman_tree(txt.as_bytes(), hm);
+        let buffer = &mut Vec::new();
+        huffman::decode_huffman_tree(&encoded, &root, buffer);
+        assert_eq!(txt, from_utf8(buffer).unwrap());
     }
 }
